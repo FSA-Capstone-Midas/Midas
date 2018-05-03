29da4aa13a90bf28b587176c6294d544db50c41d
Philip_Leung <philip.leung90@gmail.com>
2018-05-03 11:55:23 -0400

    added user database model

--- server/api/users.js
+++ server/api/users.js
@@ -1,14 +1,23 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require("express").Router();
+const { User } = require("../db/models");
+module.exports = router;
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ["id", "email"]
   })
     .then(users => res.json(users))
-    .catch(next)
-})
+    .catch(next);
+});
+
+//user log in , return user infos
+router.get("/user", (req, res, next) => {});
+
+// user sign up new account
+router.post("/user", (req, res, next) => {});
+
+// user edit info
+router.put("/user", (req, res, next) => {});
