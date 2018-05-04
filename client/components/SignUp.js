197eb5adbe61f6e6653f7c0a40475b93e537e750
Philip_Leung <philip.leung90@gmail.com>
2018-05-04 12:24:32 -0400

    login page successfully rendering

--- client/components/SignUp.js
+++ client/components/SignUp.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Carousel from "nuka-carousel";
 import Footer from "./Footer";
 import {
@@ -21,156 +22,6 @@ import { NavLink, withRouter } from "react-router-dom";
 import MobileContainer from "./MobileContainer";
 import DesktopContainer from "./DesktopContainer";
 
-/* eslint-disable react/no-multi-comp */
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-// class DesktopContainer extends Component {
-//   state = {};
-
-//   hideFixedMenu = () => this.setState({ fixed: false });
-//   showFixedMenu = () => this.setState({ fixed: true });
-
-//   render() {
-//     const { children } = this.props;
-//     const { fixed } = this.state;
-
-//     return (
-//       <Responsive {...Responsive.onlyComputer}>
-//         <Visibility
-//           once={false}
-//           onBottomPassed={this.showFixedMenu}
-//           onBottomPassedReverse={this.hideFixedMenu}
-//         >
-//           <Segment
-//             inverted
-//             textAlign="center"
-//             style={{ minHeight: 100, padding: "1em 0em" }}
-//             vertical
-//           >
-//             <Menu
-//               fixed={fixed ? "top" : null}
-//               inverted={!fixed}
-//               pointing={!fixed}
-//               secondary={!fixed}
-//               size="large"
-//             >
-//               <Container>
-//                 <Menu.Item exact as={NavLink} to="/">
-//                   Home
-//                 </Menu.Item>
-//                 <Menu.Item exact as={NavLink} to="/howitworks">
-//                   How It Works
-//                 </Menu.Item>
-//                 <Menu.Item as="a">Security</Menu.Item>
-//                 <Menu.Item as="a">Blog</Menu.Item>
-//                 <Menu.Item as="a">Support</Menu.Item>
-//                 <Menu.Item as={NavLink} to="/aboutus">
-//                   About Us
-//                 </Menu.Item>
-//                 <Menu.Item position="right">
-//                   <Button as="a" inverted={!fixed}>
-//                     Log in
-//                   </Button>
-//                 </Menu.Item>
-//               </Container>
-//             </Menu>
-//           </Segment>
-//         </Visibility>
-
-//         {children}
-//       </Responsive>
-//     );
-//   }
-// }
-
-// DesktopContainer.propTypes = {
-//   children: PropTypes.node
-// };
-
-// class MobileContainer extends Component {
-//   state = {};
-
-//   handlePusherClick = () => {
-//     const { sidebarOpened } = this.state;
-
-//     if (sidebarOpened) this.setState({ sidebarOpened: false });
-//   };
-
-//   handleToggle = () =>
-//     this.setState({ sidebarOpened: !this.state.sidebarOpened });
-
-//   render() {
-//     const { children } = this.props;
-//     const { sidebarOpened } = this.state;
-
-//     return (
-//       <Responsive {...Responsive.onlyMobile}>
-//         <Sidebar.Pushable>
-//           <Sidebar
-//             as={Menu}
-//             animation="uncover"
-//             inverted
-//             vertical
-//             visible={sidebarOpened}
-//           >
-//             <Menu.Item as="a" active>
-//               Home
-//             </Menu.Item>
-//             <Menu.Item as="a">How It Works</Menu.Item>
-//             <Menu.Item as="a">Security</Menu.Item>
-//             <Menu.Item as="a">Blog</Menu.Item>
-//             <Menu.Item as="a">Support</Menu.Item>
-//             <Menu.Item as="a">Log in</Menu.Item>
-//           </Sidebar>
-
-//           <Sidebar.Pusher
-//             dimmed={sidebarOpened}
-//             onClick={this.handlePusherClick}
-//             style={{ minHeight: "100vh" }}
-//           >
-//             <Segment
-//               inverted
-//               textAlign="center"
-//               style={{ minHeight: 350, padding: "1em 0em" }}
-//               vertical
-//             >
-//               <Container>
-//                 <Menu inverted pointing secondary size="large">
-//                   <Menu.Item onClick={this.handleToggle}>
-//                     <Icon name="sidebar" />
-//                   </Menu.Item>
-//                   <Menu.Item position="right">
-//                     <Button as="a" inverted>
-//                       Log in
-//                     </Button>
-//                     <Button
-//                       as={NavLink}
-//                       to="/signup"
-//                       inverted
-//                       style={{ marginLeft: "0.5em" }}
-//                     >
-//                       Sign Up
-//                     </Button>
-//                   </Menu.Item>
-//                 </Menu>
-//               </Container>
-//             </Segment>
-
-//             {children}
-//           </Sidebar.Pusher>
-//         </Sidebar.Pushable>
-//       </Responsive>
-//     );
-//   }
-// }
-
-// MobileContainer.propTypes = {
-//   children: PropTypes.node
-// };
-
 const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
@@ -187,6 +38,7 @@ class SignUp extends Component {
     super(props);
   }
   render() {
+    const { name, displayName, handleSubmit, error } = props;
     const startYear = 1948;
     const endYear = 2000;
     const startDay = 1;
@@ -218,13 +70,12 @@ class SignUp extends Component {
             </Grid.Row>
           </Grid>
         </Segment>
-
         <Segment style={{ padding: "2em 0em" }} vertical>
           <Grid container stackable verticalAlign="middle">
             <Grid.Row>
               <Grid.Column>
                 <div id="signup-login">
-                  <form className="ui form">
+                  <form className="ui form" handleSubmit={handleSubmit}>
                     <div className="field">
                       <label>First Name</label>
                       <input placeholder="First Name" />
@@ -307,4 +158,43 @@ class SignUp extends Component {
     );
   }
 }
-export default withRouter(SignUp);
+
+/**
+ * CONTAINER
+ *   Note that we have two different sets of 'mapStateToProps' functions -
+ *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+ *   function, and share the same Component. This is a good example of how we
+ *   can stay DRY with interfaces that are very similar to each other!
+ */
+
+const mapSignup = state => {
+  return {
+    name: "signup",
+    displayName: "Sign Up",
+    error: state.user.error
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const email = evt.target.email.value;
+      const password = evt.target.password.value;
+      dispatch(auth(email, password, formName));
+    }
+  };
+};
+
+export const Signup = connect(mapSignup, mapDispatch)(SignUp);
+
+/**
+ * PROP TYPES
+ */
+SignUp.propTypes = {
+  name: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object
+};
