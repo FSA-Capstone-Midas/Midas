957671ec46503fd5783f74c65227050f1875be3d
joshua park <jiyong11281@gmail.com>
2018-05-04 18:34:36 -0400

    fixed conflict

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React, { Component } from "react";
 import Carousel from "nuka-carousel";
 import Footer from "./Footer";
+import { connect } from "react-redux";
 import {
   Button,
   Container,
