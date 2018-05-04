18250683f96e8e6f8f97b832eb3c4836c669e5b0
Philip_Leung <philip.leung90@gmail.com>
2018-05-03 22:29:11 -0400

    added victory chart

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import Carousel from "nuka-carousel";
 import Footer from "./Footer";
 import {
@@ -20,6 +21,13 @@ import {
 import { NavLink, withRouter } from "react-router-dom";
 import DesktopContainer from "./AfterLogin/AfterLoginDesktopContainer";
 import MobileContainer from "./AfterLogin/AfterLoginMobileContainer";
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryStack
+} from "victory";
 
 const ResponsiveContainer = ({ children }) => (
   <div>
@@ -32,10 +40,51 @@ ResponsiveContainer.propTypes = {
   children: PropTypes.node
 };
 
+const data2012 = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 }
+];
+
+const data2013 = [
+  { quarter: 1, earnings: 15000 },
+  { quarter: 2, earnings: 12500 },
+  { quarter: 3, earnings: 19500 },
+  { quarter: 4, earnings: 13000 }
+];
+
+const data2014 = [
+  { quarter: 1, earnings: 11500 },
+  { quarter: 2, earnings: 13250 },
+  { quarter: 3, earnings: 20000 },
+  { quarter: 4, earnings: 15500 }
+];
+
+const data2015 = [
+  { quarter: 1, earnings: 18000 },
+  { quarter: 2, earnings: 13250 },
+  { quarter: 3, earnings: 15000 },
+  { quarter: 4, earnings: 12000 }
+];
+
 const UserHome = () => (
   <ResponsiveContainer>
     <Segment>
       <h1>Pie chart (RIght)</h1>
+      <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        />
+        <VictoryAxis dependentAxis tickFormat={x => `$${x / 1000}k`} />
+        <VictoryStack colorScale={"warm"}>
+          <VictoryBar data={data2012} x="quarter" y="earnings" />
+          <VictoryBar data={data2013} x="quarter" y="earnings" />
+          <VictoryBar data={data2014} x="quarter" y="earnings" />
+          <VictoryBar data={data2015} x="quarter" y="earnings" />
+        </VictoryStack>
+      </VictoryChart>
     </Segment>
     <Segment>
       <h1>Account info (LEFT)</h1>
