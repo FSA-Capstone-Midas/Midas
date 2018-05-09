1a25da2e1dda2aaea82e8d5ed8e50d7ecfd5bb9e
Philip803 <30559200+Philip803@users.noreply.github.com>
2018-05-08 20:56:08 -0400

    first commit for trends

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,4 +1,3 @@
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Carousel from "nuka-carousel";
@@ -6,32 +5,10 @@ import Footer from "./Footer";
 import Loading from "./Loading";
 import Plaid from "./Plaid";
 import AccountTable from "./AccountTable";
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility
-} from "semantic-ui-react";
+import { Segment } from "semantic-ui-react";
 import { NavLink, withRouter } from "react-router-dom";
 import DesktopContainer from "./AfterLogin/AfterLoginDesktopContainer";
 import MobileContainer from "./AfterLogin/AfterLoginMobileContainer";
-import {
-  VictoryBar,
-  VictoryChart,
-  VictoryAxis,
-  VictoryTheme,
-  VictoryStack
-} from "victory";
-
 const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
@@ -42,34 +19,6 @@ const ResponsiveContainer = ({ children }) => (
 ResponsiveContainer.propTypes = {
   children: PropTypes.node
 };
-
-const data2012 = [
-  { quarter: 1, earnings: 13000 },
-  { quarter: 2, earnings: 16500 },
-  { quarter: 3, earnings: 14250 },
-  { quarter: 4, earnings: 19000 }
-];
-
-const data2013 = [
-  { quarter: 1, earnings: 15000 },
-  { quarter: 2, earnings: 12500 },
-  { quarter: 3, earnings: 19500 },
-  { quarter: 4, earnings: 13000 }
-];
-
-const data2014 = [
-  { quarter: 1, earnings: 11500 },
-  { quarter: 2, earnings: 13250 },
-  { quarter: 3, earnings: 20000 },
-  { quarter: 4, earnings: 15500 }
-];
-
-const data2015 = [
-  { quarter: 1, earnings: 18000 },
-  { quarter: 2, earnings: 13250 },
-  { quarter: 3, earnings: 15000 },
-  { quarter: 4, earnings: 12000 }
-];
 
 class UserHome extends React.Component {
   constructor(props) {
@@ -139,10 +88,3 @@ const mapState = state => {
 };
 
 export default connect(mapState)(UserHome);
-
-/**
- * PROP TYPES
- */
-UserHome.propTypes = {
-  email: PropTypes.string
-};
