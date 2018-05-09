4e8fcda0b391210a28df527b88e93da217e424d7
Philip_Leung <philip.leung90@gmail.com>
2018-05-09 15:58:05 -0400

    added MM change rent data map

--- client/components/DataMapApartmentsMMRentChange.js
+++ client/components/DataMapApartmentsMMRentChange.js
@@ -37,203 +37,203 @@ class DatMapApartmentsMMRentChange extends Component {
       data: {
         AZ: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         CO: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         DE: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.1%"
         },
         FL: {
           fillKey: "UNDECIDED",
-          info: "$322,398"
+          info: "0.1%"
         },
         GA: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         HI: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.5%"
         },
         ID: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.3%"
         },
         IL: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0%"
         },
         IN: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0%"
         },
         IA: {
           fillKey: "Light Democrat",
-          info: "$322,398"
+          info: "-0.2%"
         },
         KS: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.2%"
         },
         KY: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         LA: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.4%"
         },
         MD: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.1%"
         },
         ME: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.5%"
         },
         MA: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.7%"
         },
         MN: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.3%"
         },
         MI: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.1%"
         },
         MS: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.1%"
         },
         MO: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.2%"
         },
         MT: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.3%"
         },
         NC: {
           fillKey: "Light Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         NE: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         NV: {
           fillKey: "Heavy Democrat",
-          info: "$322,398"
+          info: "0.3%"
         },
         NH: {
           fillKey: "Light Democrat",
-          info: "$322,398"
+          info: "0%"
         },
         NJ: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         NY: {
           fillKey: "Very High",
-          info: "$322,398"
+          info: "0%"
         },
         ND: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.6%"
         },
         NM: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.4%"
         },
         OH: {
           fillKey: "UNDECIDED",
-          info: "$322,398"
+          info: "0%"
         },
         OK: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.1%"
         },
         OR: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.7%"
         },
         PA: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.2%"
         },
         RI: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.9%"
         },
         SC: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         SD: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.2%"
         },
         TN: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0%"
         },
         TX: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.1%"
         },
         UT: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0%"
         },
         WI: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0%"
         },
         VA: {
           fillKey: "Light Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         VT: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.2%"
         },
         WA: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "-0.8%"
         },
         WV: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.4%"
         },
         WY: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.2%"
         },
         CA: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         CT: {
           fillKey: "Democrat",
-          info: "$322,398"
+          info: "0.2%"
         },
         AK: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.3%"
         },
         AR: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "0.1%"
         },
         AL: {
           fillKey: "Republican",
-          info: "$322,398"
+          info: "-0.5%"
         }
       },
       done: function(map) {
