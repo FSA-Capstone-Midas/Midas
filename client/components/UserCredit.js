e404fea1138f9ea2bf1d13be95d3e44ed39fd117
Philip803 <30559200+Philip803@users.noreply.github.com>
2018-05-11 23:08:57 -0400

    added container on credit score page

--- client/components/UserCredit.js
+++ client/components/UserCredit.js
@@ -22,56 +22,61 @@ const UserCredit = () => (
   <ResponsiveContainer>
     <Container>
       <Segment>
-        <div
-          className="ui segment"
-          style={{ marginLeft: "15%", marginRight: "15%" }}
-        >
+        <Segment style={{ fontSize: "28px", textAlign: "center" }}>
+          Credit Score
+        </Segment>
+        <Segment>
           <div
-            className="ui statistic"
-            style={{
-              width: "-webkit-fill-available",
-              marginLeft: "auto",
-              marginRight: "auto"
-            }}
+            className="ui segment"
+            style={{ marginLeft: "15%", marginRight: "15%" }}
           >
-            <h2
-              className="ui header"
+            <div
+              className="ui statistic"
               style={{
+                width: "-webkit-fill-available",
                 marginLeft: "auto",
                 marginRight: "auto"
               }}
             >
-              The average Midas credit score is{" "}
-            </h2>
-            <div className="ui red statistic">
-              <div className="value">800</div>
+              <h2
+                className="ui header"
+                style={{
+                  marginLeft: "auto",
+                  marginRight: "auto"
+                }}
+              >
+                The average Midas credit score is{" "}
+              </h2>
+              <div className="ui red statistic">
+                <div className="value">800</div>
+              </div>
+              <ProgressBar>
+                {" "}
+                <ProgressBar striped bsStyle="danger" now={20} key={1} />{" "}
+                <ProgressBar bsStyle="warning" now={30} key={2} />{" "}
+                <ProgressBar active bsStyle="success" now={30} key={3} />{" "}
+              </ProgressBar>
+              <a
+                href="https://google.com"
+                style={{
+                  marginLeft: "auto",
+                  marginRight: "auto"
+                }}
+              >
+                <h2> Feel free to sign up to check your Credit score!</h2>
+              </a>
             </div>
-            <ProgressBar>
-              {" "}
-              <ProgressBar striped bsStyle="danger" now={20} key={1} />{" "}
-              <ProgressBar bsStyle="warning" now={30} key={2} />{" "}
-              <ProgressBar active bsStyle="success" now={30} key={3} />{" "}
-            </ProgressBar>
-            <a
-              href="https://google.com"
-              style={{
-                marginLeft: "auto",
-                marginRight: "auto"
-              }}
-            >
-              <h2> Feel free to sign up to check your Credit score!</h2>
-            </a>
           </div>
-        </div>
-        <div
-          style={{
-            position: "fixed",
-            left: "0",
-            bottom: "0",
-            width: "100%",
-            textAlign: "center"
-          }}
-        />
+          <div
+            style={{
+              position: "fixed",
+              left: "0",
+              bottom: "0",
+              width: "100%",
+              textAlign: "center"
+            }}
+          />
+        </Segment>
       </Segment>
     </Container>
     <Footer />
