09367dcf849c63f30f7f445025a9b719febeda8e
Casey Chan <caseygocoding@gmail.com>
2018-05-13 15:07:53 -0400

    fix the mobile version for about us so that it is responsive

--- client/components/BeforeLoginPages/AboutUs.js
+++ client/components/BeforeLoginPages/AboutUs.js
@@ -2,7 +2,15 @@ import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
-import { Button, Grid, Header, Segment } from "semantic-ui-react";
+import {
+  Button,
+  Grid,
+  Header,
+  Segment,
+  Card,
+  Icon,
+  Image,
+} from "semantic-ui-react";
 import MobileContainer from "../BeforeLogin/MobileContainer";
 import DesktopContainer from "../BeforeLogin/DesktopContainer";
 import AfterDesktopContainer from "../AfterLogin/AfterLoginDesktopContainer";
@@ -53,88 +61,180 @@ const AboutUs = props => {
         style={{ padding: "2em 0em", fontSize: "12px", textAlign: "center" }}
         vertical
       >
-        <Grid container stackable verticalAlign="middle">
+        <Grid container stackable verticalAlign="middle" columns={4}>
           <Grid.Row>
             <Grid.Column>
-              <div className="ui link cards">
-                <div className="card">
-                  <div className="image">
-                    <a href="https://www.linkedin.com/in/jingjing-li-757b0711/">
-                      <img src="../../../../../../pictures/JJ.png" />
-                    </a>
-                  </div>
-                  <div className="content">
-                    <div className="header">JingJing Li</div>
-                    <div className="meta" />
-                    <div className="description">Software Developer</div>
-                  </div>
-                  <div className="extra content">
-                    <span className="right floated">Joined in 2018</span>
-                    <span>
-                      <i className="user icon" />
-                      1000 connection
-                    </span>
-                  </div>
+              <Card centered>
+                <Image src="../../../../../../pictures/JJ.png" />
+                <Card.Content>
+                  <Card.Header>JingJing Li</Card.Header>
+                  <Card.Meta>Joined in 2018</Card.Meta>
+                  <Card.Description>Software Developer</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/jingjing-li-757b0711/"
+                  >
+                    <Icon name="user" />
+                    2000 Connections
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Card centered>
+                <Image src="../../../../../../pictures/Joshua.png" />
+                <Card.Content>
+                  <Card.Header>Joshua Park</Card.Header>
+                  <Card.Meta>Joined in 2018</Card.Meta>
+                  <Card.Description>Software Developer</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/joshua-park-7044a7153/"
+                  >
+                    <Icon name="user" />
+                    2000 Connections
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Card centered>
+                <Image src="../../../../../../pictures/Philip.png" />
+                <Card.Content>
+                  <Card.Header>Philip Leung</Card.Header>
+                  <Card.Meta>Joined in 2018</Card.Meta>
+                  <Card.Description>Software Developer</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/philipleung90/"
+                  >
+                    <Icon name="user" />
+                    2000 Connections
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Card centered>
+                <Image src="../../../../../../pictures/Casey.png" />
+                <Card.Content>
+                  <Card.Header>Casey Chan</Card.Header>
+                  <Card.Meta>Joined in 2018</Card.Meta>
+                  <Card.Description>Software Developer</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/whchan703/"
+                  >
+                    <Icon name="user" />
+                    2000 Connections
+                  </a>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+
+            {/* <Grid.Column className="ui link cards" width={4}> */}
+            {/* <div className="ui link cards"> */}
+            {/* <div className="card">
+                <div className="image">
+                  <a href="https://www.linkedin.com/in/jingjing-li-757b0711/">
+                    <img src="../../../../../../pictures/JJ.png" />
+                  </a>
                 </div>
-                <div className="card">
-                  <div className="image">
-                    <a href="https://www.linkedin.com/in/joshua-park-7044a7153/">
-                      <img src="../../../../../../pictures/Joshua.png" />
-                    </a>
-                  </div>
-                  <div className="content">
-                    <div className="header">Joshua Park</div>
-                    <div className="meta" />
-                    <div className="description">Software Developer</div>
-                  </div>
-                  <div className="extra content">
-                    <span className="right floated">Joined in 2018</span>
-                    <span>
-                      <i className="user icon" />
-                      20,000 connections
-                    </span>
-                  </div>
+                <div className="content">
+                  <div className="header">JingJing Li</div>
+                  <div className="meta" />
+                  <div className="description">Software Developer</div>
                 </div>
-                <div className="card">
-                  <div className="image">
-                    <a href="https://www.linkedin.com/in/philipleung90/">
-                      <img src="../../../../../../pictures/Philip.png" />
-                    </a>
-                  </div>
-                  <div className="content">
-                    <div className="header">Philip Leung</div>
-                    <div className="meta" />
-                    <div className="description">Software Developer</div>
-                  </div>
-                  <div className="extra content">
-                    <span className="right floated">Joined in 2018</span>
-                    <span>
-                      <i className="user icon" />
-                      3,000 connections
-                    </span>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="image">
-                    <a href="https://www.linkedin.com/in/whchan703//">
-                      <img src="../../../../../../pictures/Casey.png" />
-                    </a>
-                  </div>
-                  <div className="content">
-                    <div className="header">Casey Chan</div>
-                    <div className="meta" />
-                    <div className="description">Software Developer</div>
-                  </div>
-                  <div className="extra content">
-                    <span className="right floated">Joined in 2018</span>
-                    <span>
-                      <i className="user icon" />
-                      2,000 connections
-                    </span>
-                  </div>
+                <div className="extra content">
+                  <span className="right floated">Joined in 2018</span>
+                  <span>
+                    <i className="user icon" />
+                    1000 connection
+                  </span>
                 </div>
               </div>
             </Grid.Column>
+            <Grid.Column className="ui link cards" width={4}>
+              <div className="card">
+                <div className="image">
+                  <a href="https://www.linkedin.com/in/joshua-park-7044a7153/">
+                    <img src="../../../../../../pictures/Joshua.png" />
+                  </a>
+                </div>
+                <div className="content">
+                  <div className="header">Joshua Park</div>
+                  <div className="meta" />
+                  <div className="description">Software Developer</div>
+                </div>
+                <div className="extra content">
+                  <span className="right floated">Joined in 2018</span>
+                  <span>
+                    <i className="user icon" />
+                    20,000 connections
+                  </span>
+                </div>
+              </div>
+            </Grid.Column>
+            <Grid.Column className="ui link cards" width={4}>
+              <div className="card">
+                <div className="image">
+                  <a href="https://www.linkedin.com/in/philipleung90/">
+                    <img src="../../../../../../pictures/Philip.png" />
+                  </a>
+                </div>
+                <div className="content">
+                  <div className="header">Philip Leung</div>
+                  <div className="meta" />
+                  <div className="description">Software Developer</div>
+                </div>
+                <div className="extra content">
+                  <span className="right floated">Joined in 2018</span>
+                  <span>
+                    <i className="user icon" />
+                    3,000 connections
+                  </span>
+                </div>
+              </div>
+            </Grid.Column>
+
+            <Grid.Column className="ui link cards" width={4}>
+              <div className="card">
+                <div className="image">
+                  <a href="https://www.linkedin.com/in/whchan703/">
+                    <img src="../../../../../../pictures/Casey.png" />
+                  </a>
+                </div>
+                <div className="content">
+                  <div className="header">Casey Chan</div>
+                  <div className="meta" />
+                  <div className="description">Software Developer</div>
+                </div>
+                <div className="extra content">
+                  <span className="right floated">Joined in 2018</span>
+                  <span>
+                    <i className="user icon" />
+                    2,000 connections
+                  </span>
+                </div>
+              </div> */}
+
+            {/* </div> */}
+            {/* </Grid.Column> */}
           </Grid.Row>
         </Grid>
       </Segment>
