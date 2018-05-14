225016178c3cc81caa62e1f648b160c8a44b7025
joshua park <jiyong11281@gmail.com>
2018-05-14 16:24:14 -0400

    finished with bills

--- client/components/Bills.js
+++ client/components/Bills.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import DesktopContainer from "./AfterLogin/AfterLoginDesktopContainer";
 import MobileContainer from "./AfterLogin/AfterLoginMobileContainer";
 import { connect } from "react-redux";
-import Footer from "./Footer";
-import { Segment, Button } from "semantic-ui-react";
 import BillForm from "./BillForm";
-import BillAlert from "./BillAlert";
 
 const ResponsiveContainer = ({ children }) => (
   <div>
@@ -25,7 +22,6 @@ class Bills extends Component {
 }
 
 const mapState = state => {
-  //set up number of goals of each user
   return {};
 };
 
